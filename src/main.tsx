96997c72f3634d3f6c3d528ce71f3e613e479016
nice2008 <t09n09a08@gmail.com>
2025-09-13 18:58:19 +0500

    add new func

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { routesConfig } from "./config/routes";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
-import './index.css'
+import './index.scss'
 
 const router = createBrowserRouter(routesConfig);
 
